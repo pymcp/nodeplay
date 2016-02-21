e24debf8b4febdbba077a6b8519837908eb84388
Mike Patterson <mike@pymcp.net>
2016-02-21 10:24:19 -0800

    Chat functionailty works

--- chat/index.js
+++ chat/index.js
@@ -13,6 +13,7 @@ io.on('connection', function(socket) {
     });
     socket.on('chat message', function(msg) {
         console.log('Message: ' + msg);
+        io.emit('chat message', msg);
     });
 });
 
